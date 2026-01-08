fe79686d4f22caec91a284c31e4b56b76e7a04b6
andrewbaas <61301494+andrewbaas@users.noreply.github.com>
2026-01-08 16:01:43 -0600

    Added projects to portfolio and organized filters

--- docs/portfolio.md
+++ docs/portfolio.md
@@ -1,3 +1,7 @@
 # Portfolio
 
+Below, I've included a list of projects in which I have made **significant software contributions** (or am the sole developer) along with a brief summary and links to relevant resources.
+
+<details><summary>Note for Recruiters:</summary> The table below only includes projects with a clear software product. Thus, work experience like HPC benchmarking, scripts generated for data processing, and auxiliary front-end development are not included. If you are interested in a comprehensive list, please contact me on <a href="https://www.linkedin.com/in/andrew-baas/">LinkedIn</a> for a copy of my CV.</details>
+
 {{ portfolio_table() }}
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import yaml
-import re
+import time
 
 def define_env(env):
     """
     This is the hook for the variables, macros and filters.
     """
+
+    skillsCategories = {
+        "Scripting": ["Python", "Julia", "Java", "Go", "Bash", "R"],
+        "Web Dev.": ["HTML", "CSS", "React", "Typescript", "Zustand", "Markdown"],
+        "Databases": ["PostgreSQL", "Solr", "Django"],
+        "Data Analysis": ["Pandas", "SKLearn", "Plotly", "Graph Analysis"],
+        "Skills": ["Simulation", "HPC", "Multiphysics", "Epidemiology", "Desmos CL"]
+    }
 
     @env.macro
     def portfolio_table():
@@ -13,28 +21,41 @@ def define_env(env):
         with open(f"docs/data/projects.yaml") as f:
             data = yaml.safe_load(f)
         rows = []
+        dates = []
         for project in data:
+            project_image = project["image"] if "image" in project else "default_img.svg"
             project_dates = f"{project["dates"]["start"]} - {project["dates"]["end"]}"
             project_desc = description_container + \
-                f'<div><img style="max-width:120px" src="../assets/project_images/{project["image"]}"/></div>' + \
+                f'<div><img style="max-width:120px; min-width:120px" src="../assets/project_images/{project_image}"/></div>' + \
                     "<div></div>" +\
                     "<div>" + \
                         f"<strong>{project["name"]}</strong><br/>" + \
                         (f"<a href={project["url"]["link"]}>{project["url"]["name"]}</a><br/>" if "url" in project.keys() else "") + \
                         project["description"] + \
                     "</div></div>"
-            print(project["description"])
             skills = project["skills"]
             project_skills = ", ".join([s["name"] for s in skills])
+            dates.append(time.strptime(project["dates"]["start"], "%m/%Y"))
             rows.append(f"| {project_dates} | {project_desc} | {project_skills} |")
-        return makeButtonList(data) + "\n" + header + "\n" + "\n".join(rows)
+        rowsAndDates = sorted(zip(rows, dates), key=lambda x: x[1], reverse=True)
+        sortedRows, _ = zip(*rowsAndDates)
+        return makeButtonList(data) + "\n" + header + "\n" + "\n".join(sortedRows)
     
     def makeButtonList(data):
         skills = set()
         for project in data:
             for skill in project["skills"]:
                 skills.add(skill["name"])
-        
-        return  '<div id="skillButtons"><label><strong>Skill filter:</strong> </label>' +\
-                "".join([f'<button class="skill" onclick="clickSkillTag(event, \'skillButtons\')">{skill}</button>' for skill in skills]) +\
-                '</div>'
+        nCats = len(skillsCategories)
+        containerOpener = f'<div id="skillButtons" style="display:grid; column-gap: 5px; grid-template-columns: {" ".join(["1fr" for i in range(nCats)])}">'
+        columns = []
+        for category, skillList in skillsCategories.items():
+            buttons = []
+            for skill in skillList:
+                skills.discard(skill)
+                buttons.append(f'<button class="skill" onclick="clickSkillTag(event, \'skillButtons\')">{skill}</button>')
+            columns.append('<div>' + f"<label><strong>{category}:</strong></label><br/>" + "".join(buttons) + '</div>')
+
+        if len(skills) != 0:
+            print("Missing skills " + ", ".join(skills))
+        return  containerOpener + "".join(columns) + "</div>"
